330cfdc576a0b3ea2f463e6cff1c0046724d804a
Liuyang <15327477@qq.com>
2019-01-22 14:46:24 +0800

    1.两个输入框样式;

---
+++ app/src/main/java/com/liuyang/androidcontrolsdev/ControlsActivity.java
@@ -0,0 +1,34 @@
+package com.liuyang.androidcontrolsdev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controls);
+
+
+        // 拾色器
+        // http://link.fobshanghai.com/rgbcolor.htm
+
+
+        // bg_edittext_a:圆角边框focus边框变色
+        // https://www.cnblogs.com/guop/p/5217534.html
+        /**
+         * android:background="@drawable/bg_edittext_a"
+         * */
+
+
+        // myThemeEdittextB:改变下划线颜色
+        // https://blog.csdn.net/qq_20451879/article/details/79075272
+        /**
+         * android:theme="@style/myThemeEdittextB"
+         * */
+
+
+
+    }
+}
---
+++ app/src/main/java/com/liuyang/androidcontrolsdev/MainActivity.java
@@ -0,0 +1,23 @@
+package com.liuyang.androidcontrolsdev;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Intent in = new Intent(this, LoginActivity.class);
+//        this.startActivity(in);
+
+
+        Intent in = new Intent(this, ControlsActivity.class);
+        this.startActivity(in);
+
+
+    }
+}
